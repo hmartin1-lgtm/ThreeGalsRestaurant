b070a6e59bcbcfee5a503c6df0e829238da8a53e
gfeuling1 <gfeuling1@students.ccu.edu>
2026-04-21 14:27:02 -0600

    Update app.js

--- Java/app.js
+++ Java/app.js
@@ -388,9 +388,7 @@ function initCheckout() {
     window.location.href = 'confirmation.html';
   });
 }
-
 function WeatherBug() {
-    fetch('https://api.weatherapi.com/v1/current.json?key=3a4515825e3c433f8dc40901220203&q=Denver&aqi=no')
     fetch('https://api.weatherapi.com/v1/current.json?key=3a4515825e3c433f8dc40901220203&q=London&aqi=no')
     .then((res) => res.json())
     .then((data) => {
